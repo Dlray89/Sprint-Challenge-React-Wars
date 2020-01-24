94647c47c51adae123c79e6ea080f1c169a853ce
David Ray <dlrayjr89@gmail.com>
2020-01-24 13:52:06 -0500

    update

---
+++ starwars/src/CharacterCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CharacterCard() {
+  return (
+    <div>
+    <App />
+      <h1>NAme</h1>
+      <p>Height </p>
+      <p>Mass</p>
+    </div>
+  );
+}
+
+export default CharacterCard;
